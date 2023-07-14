b6888c2a5dad86fe3fdfa3529a427aab669cf3f2
eunc823 <uuu3686@naver.com>
2023-07-14 17:56:43 +0900

    ㅇ

--- sql/semi-project.sql
+++ sql/semi-project.sql
@@ -10,7 +10,7 @@ alter user threego quota unlimited on users;
 -- drop user threego cascade;
 
 -- select sid, serial#, username,status from v$session where username = 'THREEGO';
--- alter system kill SESSION '375,47511';
+-- alter system kill SESSION '30,42970';
  
 ---------------------------------------------------------------
 -- drop table member;
@@ -104,9 +104,10 @@ create table location(
 create table rider(
     r_id varchar2(30),
     r_location_id varchar2(30),
-    r_status char(1),
+    r_status char(1) default '0',
     r_reg_date date default sysdate,
     up_date	date default null,
+    fileName varchar2(500) not null,
     constraints pk_r_id primary key(r_id),
     constraints fk_rider_r_id  foreign key(r_id) references member(id) on delete cascade,
     constraints fk_rider_location_id foreign key(r_location_id) references location(l_id) on delete set null,
@@ -175,6 +176,19 @@ BEGIN
 END;
 /
 
+create table msgbox(
+    msg_no number, 
+    msg_type varchar2(50) not null, 
+    msg_sender varchar2(30) not null, 
+    msg_receiver varchar2(30) not null, 
+    msg_content varchar2(4000), 
+    constraints pk_msgbox_msg_no primary key(msg_no),
+    constraints fk_msgbox_msg_sender foreign key(msg_sender) references member(id) on delete cascade,
+    constraints ck_msgbox_msg_type check(msg_type in('C', 'A', 'P'))
+    -- c 는 조치 ,  a 는 승인 알람,  p는 진행상황알람 
+);
+create sequence seq_msg_no;
+
  insert into member values (
     'admin', 'admin','관리자','admin@admin1.com','01033233372','A','11111' ,'관리자입니다.',default
 );   
@@ -212,7 +226,7 @@ insert into location values(
 );
 
  insert into rider values (
-    'xogus', 'S2','1', sysdate,sysdate
+    'xogus', 'S2','1', sysdate,sysdate, 'asdasd'
 );
 
  insert into request values(
@@ -236,7 +250,3 @@ select * from payment;
 select * from del_member;
     -- commit;
 
-SELECT sum(p_cnt)
-FROM payment
-WHERE p_date >= TO_DATE('23/07/01', 'YY/MM/DD')
-  AND p_date <= TO_DATE('23/07/14', 'YY/MM/DD');
